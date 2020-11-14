472655496b7bfcf8f80b0696286bd7e906d434b0
Harry <2411mail@gmail.com>
2020-11-14 21:24:02 +0800

    connect with contract

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
 import "antd/dist/antd.css";
-import Create from './Create';
-import List from './List';
+import Create from './components/Create';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>套娃协议</h2>
-      <h3>无限资产的无限合成 </h3>
-      <div>
-        <Create />
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>套娃协议</h2>
+//       <h3>无限资产的无限合成 </h3>
+//       <div>
+//         <Create />
+//       </div>
+//       <div>
+//         <List />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Web3 from "web3";
+import getWeb3 from "./util/getWeb3";
+
+const appStyles = {}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      web3: null,
+    };
+  }
+
+  async componentWillMount() {
+    const web3 = await getWeb3();
+    this.setState({
+      web3,
+    });
+  }
+
+  render() {
+    return (
+      <div className={appStyles.app}>
+        <div className={appStyles.appHeader}>
+          <h2>套娃协议</h2>
+          <p>无限资产的无限合成</p>
+        </div>
+        <div className={appStyles.appIntro}>
+          {this.state.web3 ? (
+            <div>
+              <p>
+                Provider is MetaMask: {(this.state.web3.currentProvider).isMetaMask ? "yes" : "no"}
+              </p>
+              <p>
+                Provider is Mist: {(window).mist ? "yes" : "no"}
+              </p>
+              {(this.state.web3.currentProvider).host ?
+                <p>Provider is {(this.state.web3.currentProvider).host}</p> : null}
+            </div>
+          ) :
+            <p>Web3 is loading</p>}
+        </div>
+        <hr />
+        {this.state.web3 ? <Create web3={this.state.web3} /> : null}
+        {this.state.web3 ? <List web3={this.state.web3} /> : null}
       </div>
-      <div>
-        <List />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Create.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { Form, Input, Button, Space } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import * as TruffleContract from 'truffle-contract';
+// import * as Web3 from "web3";
+const Web3 = require('web3');
+// import BigNumber from "bignumber.js"
+const BigNumber = require('bignumber.js');
+
+const MetaCoinContract = TruffleContract(require("../XSGTReward.json"));
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      accountError: false,
+      balance: "",
+      contractAddress: "",
+    };
+  }
+
+  async componentWillMount() {
+    if (this.props.web3.eth.accounts.length === 0) {
+      this.setState({
+        account: "",
+        accountError: true,
+      });
+      return;
+    }
+    MetaCoinContract.setProvider(this.props.web3.currentProvider);
+    let instance;
+    try {
+      instance = await MetaCoinContract.deployed();
+      console.log('instance: ', instance)
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+
+    const balance  = await instance.getBalance(this.props.web3.eth.accounts[0]);
+    this.setState({
+      account: this.props.web3.eth.accounts[0],
+      accountError: false,
+      balance: balance.toString(),
+      contractAddress: instance.address,
+    });
+  }
+
+  render() {
+    const onFinish = values => {
+      console.log('Received values of form:', values);
+    };
+
+    return (
+      <div>
+        <div>
+          <h3>MetaCoin</h3>
+          <p>Contract address: {this.state.contractAddress}</p>
+          <p>Account: {this.state.accountError ? "No accounts found" : this.state.account}</p>
+          <p>Balance: {this.state.balance}</p>
+        </div>
+        <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Symbol"
+            name="symbol"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.List name="tokens">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map(field => (
+                  <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                    <Form.Item
+                      {...field}
+                      label="Address"
+                      name={[field.name, 'first']}
+                      fieldKey={[field.fieldKey, 'first']}
+                      rules={[{ required: true, message: 'Missing Address' }]}
+                    >
+                      <Input placeholder="Address" />
+                    </Form.Item>
+                    <Form.Item
+                      {...field}
+                      label="Amount"
+                      name={[field.name, 'last']}
+                      fieldKey={[field.fieldKey, 'last']}
+                      rules={[{ required: true, message: 'Missing Amount' }]}
+                    >
+                      <Input placeholder="Amount" />
+                    </Form.Item>
+                    <MinusCircleOutlined onClick={() => remove(field.name)} />
+                  </Space>
+                ))}
+                <Form.Item>
+                  <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                    Add field
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+
+  }
+};
